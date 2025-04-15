608adc0ee5bd4498cfc4a48c04f1b6dc2681455f
Nandita <129239412+NReddy-15@users.noreply.github.com>
2025-04-15 11:46:20 -0400

    Implemented tests for adding items to unique collection and function for clearing based on prewritten test

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -16,14 +16,26 @@ class UniqueCollectionTest {
     // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
-
+        collection.addItem(Item("item1"))
+        collection.addItem(Item("item2"))
+        collection.addItem(Item("item3"))
+        assert(collection.size()==3)
+        assert(collection.get(0).name=="item1")
+        assert(collection.get(1).name=="item2")
+        assert(collection.get(2).name=="item3")
     }
 
     // TODO 2: Write a test to ensure that only unique items can be added to the collection
     // Uniqueness is determined by the Item.name property, which is set via the constructor
     @Test
     fun addUniqueItem() {
-
+        collection.addItem(Item("item1"))
+        collection.addItem(Item("Item1"))
+        collection.addItem(Item("item2"))
+        collection.addItem(Item("ITEM1"))
+        assert(collection.size()==2)
+        assert(collection.get(0).name == "item1")
+        assert(collection.get(1).name == "item2")
     }
 
     // Test Driven Development (TDD) test - complete specified function so that this test passes
